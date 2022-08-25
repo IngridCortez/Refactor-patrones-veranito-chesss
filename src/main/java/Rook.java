190c5f31183996058fd7b143348d2806279f8443
IngridCortez <ingrid.cortez@unmsm.edu.pe>
2022-08-25 15:13:48 -0500

    Cambiando a expresiones regulares

--- src/main/java/Rook.java
+++ src/main/java/Rook.java
@@ -12,7 +12,7 @@ import java.util.Map;
  * @version 2010.11.17
  */
 public class Rook extends ChessGamePiece{
-	protected Map<Integer, String> icons_map;
+	protected Map<Integer, String> iconsMap;
 	// private ArrayList<String> possibleMoves;
 	// ----------------------------------------------------------
 	/**
@@ -55,12 +55,12 @@ public class Rook extends ChessGamePiece{
 	 */
 	@Override
 	public ImageIcon createImageByPieceType() {
-		icons_map = new HashMap<Integer, String>();
+		iconsMap = new HashMap<Integer, String>();
 
-		icons_map.put(-1, "chessImages/default-Unassigned.gif");
-		icons_map.put(ChessGamePiece.BLACK, "chessImages/BlackRook.gif");
-		icons_map.put(ChessGamePiece.WHITE, "chessImages/WhiteRook.gif");
+		iconsMap.put(-1, "chessImages/default-Unassigned.gif");
+		iconsMap.put(ChessGamePiece.BLACK, "chessImages/BlackRook.gif");
+		iconsMap.put(ChessGamePiece.WHITE, "chessImages/WhiteRook.gif");
 		
-		return new ImageIcon(getClass().getResource(this.icons_map.get(getColorOfPiece())));
+		return new ImageIcon(getClass().getResource(this.iconsMap.get(getColorOfPiece())));
 	}
 }
